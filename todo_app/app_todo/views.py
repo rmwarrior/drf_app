9b9bd4b482715b3bc69d433273cc25a4820178d9
Denis <rmwarrior@mail.ru>
2022-04-07 00:47:33 +0500

    Lesson 04. Views. Filtering. Pagination

--- todo_app/app_todo/views.py
+++ todo_app/app_todo/views.py
@@ -1,5 +1,8 @@
+from rest_framework import status
 from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+from .filters import ToDoFilter
 from .models import Project, ToDo
 from .serializers import ProjectModelSerializer, ToDoModelSerializer
 
@@ -13,6 +16,13 @@ class ProjectModelViewSet(ModelViewSet):
     serializer_class = ProjectModelSerializer
     pagination_class = ProjectPagination
 
+    def get_queryset(self):
+        name = self.request.query_params.get('name', '')
+        projects = Project.objects.all()
+        if name:
+            projects = projects.filter(name__contains=name)
+        return projects
+
 
 class ToDoPagination(LimitOffsetPagination):
     default_limit = 20  # размер страницы 20 записей
@@ -22,3 +32,13 @@ class ToDoModelViewSet(ModelViewSet):
     queryset = ToDo.objects.all()
     serializer_class = ToDoModelSerializer
     pagination_class = ToDoPagination
+    # filterset_class = ToDoFilter  # Вариант 1
+    filterset_fields = ['project']  # Вариант 2
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance:
+            instance.is_active = False
+            instance.save()
+        # return instance
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- todo_app/todo_app/settings.py
+++ todo_app/todo_app/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'corsheaders',
     'users',
     'app_todo',
+    'django_filters',
 ]
 
 # Auth model
@@ -143,5 +144,6 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.BrowsableAPIRenderer'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    'PAGE_SIZE': 100,
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
 }
